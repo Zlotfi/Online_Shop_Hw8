5b990fffeef1dc1259f2ad10fcfb2e1151de25d7
victus <zlotfi758@gmail.com>
2023-12-18 21:49:11 +0330

    add updateUser method

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,8 +4,11 @@ import entity.User;
 import repository.UserRepository;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class UserService {
+
+    Scanner scanner = new Scanner(System.in);
 
     private final UserRepository userRepository = new UserRepository();
 
@@ -23,5 +26,21 @@ public class UserService {
     public User login(String userName) throws SQLException {
         User user = userRepository.login(userName);
         return user;
+    }
+
+    public void updateUser(int id) throws SQLException {
+        System.out.println("please enter your new name: ");
+        String name = scanner.nextLine();
+        System.out.println("please enter your new userName: ");
+        String userName = scanner.nextLine();
+        System.out.println("please enter your new email: ");
+        String email = scanner.nextLine();
+        System.out.println("please enter your new password: ");
+        String password = scanner.nextLine();
+        int result = userRepository.update(name, userName, email, password,id);
+        if (result != 0)
+            System.out.println("successfully update to database");
+        else
+            System.out.println("ERROR!");
     }
 }
