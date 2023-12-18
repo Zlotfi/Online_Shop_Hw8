e5b161dd9a228e6d383983c5796cf8bbf3742b08
victus <zlotfi758@gmail.com>
2023-12-18 19:29:24 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/Shareholder_Brand.java
+++ src/main/java/entity/Shareholder_Brand.java
@@ -1,4 +1,30 @@
 package entity;
 
 public class Shareholder_Brand {
+    private Integer brand_id;
+    private Integer shareholder_id;
+
+    public Shareholder_Brand(Integer brand_id, Integer shareholder_id) {
+        this.brand_id = brand_id;
+        this.shareholder_id = shareholder_id;
+    }
+
+    public Shareholder_Brand() {
+    }
+
+    public Integer getBrand_id() {
+        return brand_id;
+    }
+
+    public void setBrand_id(Integer brand_id) {
+        this.brand_id = brand_id;
+    }
+
+    public Integer getShareholder_id() {
+        return shareholder_id;
+    }
+
+    public void setShareholder_id(Integer shareholder_id) {
+        this.shareholder_id = shareholder_id;
+    }
 }
