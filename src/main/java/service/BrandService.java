ebaf594f46a86c0f11bd3973d5ea1c0c5b38fee0
victus <zlotfi758@gmail.com>
2023-12-18 23:31:03 +0330

    add register method

--- src/main/java/service/BrandService.java
+++ src/main/java/service/BrandService.java
@@ -1,4 +1,24 @@
 package service;
 
+import entity.Brand;
+import repository.BrandRepository;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class BrandService {
+
+    Scanner scanner = new Scanner(System.in);
+    private final BrandRepository brandRepository = new BrandRepository();
+
+    public BrandService() throws SQLException {
+    }
+
+    public void register(Brand brand) throws SQLException {
+        int result = brandRepository.save(brand);
+        if (result != 0)
+            System.out.println(brand.getName() + " successfully added to database");
+        else
+            System.out.println("ERROR");
+    }
 }
