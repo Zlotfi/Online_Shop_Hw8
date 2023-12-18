0da4528cdc94ea91a03ecc3b74e0dd63f6eb14c5
victus <zlotfi758@gmail.com>
2023-12-18 19:32:38 +0330

    add needed attribute and constructor, getter and setter

--- src/main/java/entity/Shareholder.java
+++ src/main/java/entity/Shareholder.java
@@ -1,4 +1,50 @@
 package entity;
 
 public class Shareholder {
+    private Integer id;
+    private String name;
+    private String phoneNumber;
+    private String nationalCode;
+
+    public Shareholder(Integer id, String name, String phoneNumber, String nationalCode) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+    }
+
+    public Shareholder() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
 }
