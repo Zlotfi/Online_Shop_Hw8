63e1a9a028a5ec3f6d73eab23b03af86a2a0a417
victus <zlotfi758@gmail.com>
2023-12-23 22:59:43 +0330

    add register method

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,4 +1,22 @@
 package service;
 
+import entity.Product;
+import repository.ProductRepository;
+
+import java.sql.SQLException;
+
 public class ProductService {
+
+    private final ProductRepository productRepository = new ProductRepository();
+
+    public ProductService() throws SQLException {
+    }
+
+    public void register(Product product) throws SQLException {
+        int result = productRepository.save(product);
+        if (result != 0)
+            System.out.println(product.getName() + " successfully added to database");
+        else
+            System.out.println("ERROR");
+    }
 }
