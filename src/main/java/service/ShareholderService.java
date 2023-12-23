34dae8e2db8d0fccd22e5556d38dd56cf88615d8
victus <zlotfi758@gmail.com>
2023-12-23 23:31:23 +0330

    add register method

--- src/main/java/service/ShareholderService.java
+++ src/main/java/service/ShareholderService.java
@@ -1,4 +1,22 @@
 package service;
 
+import entity.Shareholder;
+import repository.ShareholderRepository;
+
+import java.sql.SQLException;
+
 public class ShareholderService {
+
+    private final ShareholderRepository shareholderRepository = new ShareholderRepository();
+
+    public ShareholderService() throws SQLException {
+    }
+
+    public void register(Shareholder shareholder) throws SQLException {
+        int result = shareholderRepository.save(shareholder);
+        if (result != 0)
+            System.out.println(shareholder.getName() + " successfully added to database");
+        else
+            System.out.println("ERROR");
+    }
 }
