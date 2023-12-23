9b3752e8bb91493787a204606c614111fe2ea5e3
victus <zlotfi758@gmail.com>
2023-12-23 23:41:38 +0330

    add updateShareholder method

--- src/main/java/service/ShareholderService.java
+++ src/main/java/service/ShareholderService.java
@@ -4,9 +4,11 @@ import entity.Shareholder;
 import repository.ShareholderRepository;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ShareholderService {
 
+    Scanner scanner = new Scanner(System.in);
     private final ShareholderRepository shareholderRepository = new ShareholderRepository();
 
     public ShareholderService() throws SQLException {
@@ -16,6 +18,21 @@ public class ShareholderService {
         int result = shareholderRepository.save(shareholder);
         if (result != 0)
             System.out.println(shareholder.getName() + " successfully added to database");
+        else
+            System.out.println("ERROR");
+    }
+
+    public void updateShareholder(int id) throws SQLException {
+        System.out.println("please enter your new name: ");
+        String name = scanner.nextLine();
+        System.out.println("please enter your new phoneNumber: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("please enter your new nationalCode: ");
+        String nationalCode = scanner.nextLine();
+
+        int result = shareholderRepository.update(name,phoneNumber,nationalCode,id);
+        if (result != 0)
+            System.out.println("successfully update to database");
         else
             System.out.println("ERROR");
     }
