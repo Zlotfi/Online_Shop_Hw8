b87aae242734e9b138440e968311d59aa2b8cc5e
victus <zlotfi758@gmail.com>
2023-12-23 22:54:53 +0330

    add save method

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,4 +1,28 @@
 package repository;
 
+import connection.JdbcConnection;
+import entity.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ProductRepository {
+
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public ProductRepository() throws SQLException {
+    }
+
+    public int save(Product product) throws SQLException {
+        String add = "INSERT INTO product (name, createDate, category_id, brand_id) VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, product.getName());
+        preparedStatement.setString(2, product.getCreateDate());
+        preparedStatement.setInt(3,product.getCategory_id());
+        preparedStatement.setInt(4,product.getBrand_id());
+        int result = preparedStatement.executeUpdate();
+        return result;
+    }
 }
