e30021ae09ced623a39562e98467a0287aae9f79
victus <zlotfi758@gmail.com>
2023-12-27 15:01:19 +0330

    add connection to constructor with ApplicationContext

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -8,11 +8,10 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ProductRepository {
+    private final Connection connection;
 
-    JdbcConnection jdbcConnection = new JdbcConnection();
-    Connection connection = jdbcConnection.getConnection();
-
-    public ProductRepository() throws SQLException {
+    public ProductRepository(Connection connection){
+        this.connection = connection;
     }
 
     public int save(Product product) throws SQLException {
