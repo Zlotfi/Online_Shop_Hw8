20048b491beac52651db7617e2acf0935edc60bb
victus <zlotfi758@gmail.com>
2023-12-27 15:32:54 +0330

    add isValidEmailWithRegex method

--- src/main/java/utility/Validation.java
+++ src/main/java/utility/Validation.java
@@ -1,4 +1,11 @@
 package utility;
 
+import java.util.regex.Pattern;
+
 public class Validation {
+
+    public static boolean isValidEmailWithRegex(String email){
+        Pattern pattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+        return email.matches(pattern.pattern());
+    }
 }
