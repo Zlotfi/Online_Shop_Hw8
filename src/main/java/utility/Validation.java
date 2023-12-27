5eebbbc0f86a298b7204fe46f2d149ae1be4ff92
victus <zlotfi758@gmail.com>
2023-12-27 15:37:46 +0330

    add isValidPasswordWithRegex method

--- src/main/java/utility/Validation.java
+++ src/main/java/utility/Validation.java
@@ -8,4 +8,10 @@ public class Validation {
         Pattern pattern = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
         return email.matches(pattern.pattern());
     }
+
+    public static boolean isValidPasswordWithRegex(String password){
+        Pattern pattern = Pattern.compile
+                ("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,10}$");
+        return password.matches(pattern.pattern());
+    }
 }
